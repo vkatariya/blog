53902dbb0fa70c31975ca536bbb2aadb05998880
Macrosoft <vinodkatariya@gmail.com>
2019-09-18 00:23:58 +0530

    first laravel project

---
+++ resources/views/admin/partial/loginfooter.blade.php
@@ -0,0 +1,14 @@
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="{{URL::asset('vendor/jquery/jquery.min.js')}}"></script>
+  <script src="{{URL::asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="{{URL::asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="{{URL::asset('js/sb-admin-2.min.js')}}"></script>
+
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('admin.index');
+});
+Route::get('admin/login',function(){
+return view('admin.login');
+});
+Route::get('admin/register',function(){
+    return view('admin.register');
+});
+Route::get('admin/forgot',function(){
+return view('admin/forgot-password');
+});
